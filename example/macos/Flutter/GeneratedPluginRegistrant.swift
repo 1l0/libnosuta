b993cc7c1620b753255c92fb00ed6a13c467d8f8
1l0 <rh.plus001@gmail.com>
2025-07-01 16:22:02 +0900

    flutter upgrade

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import libnosuta
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LibnosutaPlugin.register(with: registry.registrar(forPlugin: "LibnosutaPlugin"))
+}
